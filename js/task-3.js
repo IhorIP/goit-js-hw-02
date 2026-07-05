3e35e2686569d197f69b06ef63b01085d643352e
Ihor_P <igorscore@gmail.com>
2026-07-05 10:25:58 +0300

    fix all js-hw-2

--- js/task-3.js
+++ js/task-3.js
@@ -1,20 +1,13 @@
-// function getElementWidth(content, padding, border) {
-//   const contentWidth = parseFloat(content);
-//   const paddingWidth = parseFloat(padding);
-//   const borderWidth = parseFloat(border);
-
-//   const totalWidth = contentWidth + (paddingWidth * 2) + (borderWidth * 2);
-
-//   return totalWidth;
-// }
-
-// console.log(getElementWidth("50px", "8px", "4px")); // 74
-// console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-// console.log(getElementWidth("200px", "0px", "0px")); // 200
-
 function checkForSpam(message) {
- 
+  const lowerCaseMessage = message.toLowerCase();
+  
+  if (lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale")) {
+    return true;
+  }
+  
+  return false;
 }
+
 
 console.log(checkForSpam("Latest technology news")); // false
 console.log(checkForSpam("JavaScript weekly newsletter")); // false
--- js/task-4.js
+++ js/task-4.js
@@ -1,11 +1,24 @@
 function getShippingCost(country) {
-//   const contentWidth = parseFloat(content);
-//   const paddingWidth = parseFloat(padding);
-//   const borderWidth = parseFloat(border);
-
-//   const totalWidth = contentWidth + (paddingWidth * 2) + (borderWidth * 2);
-
-//   return totalWidth;
+  let price;
+  
+  switch (country) {
+    case "China":
+      price = 100;
+      break;
+    case "Chile":
+      price = 250;
+      break;
+    case "Australia":
+      price = 170;
+      break;
+    case "Jamaica":
+      price = 120;
+      break;
+    default:
+      return "Sorry, there is no delivery to your country";
+  }
+  
+  return `Shipping to ${country} will cost ${price} credits`;
 }
 
 console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
